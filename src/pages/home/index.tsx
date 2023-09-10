be7d1f187dc5ec722087ae2f74f5744fa11c65f8
root <root@Arthur>
2023-09-10 19:35:24 -0300

    temp

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,30 @@
 import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
 
 export default function Home() {
   return (
-    <div></div>
+    <Container>
+      <Hero>
+        <Heading size="4xl">
+          Agendamentos descomplicados
+        </Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Preview do app"
+        />
+      </Preview>
+    </Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,27 @@
-import { styled } from '@ignite-ui/react'
+import { styled, Heading, Text } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  marginLeft: 'auto',
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+  overflow: 'hidden',
+})
 
-export const Hero = styled('div', {})
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`${Heading}`]: {
+
+  },
+
+  [`${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+    fontSize: '$xl',
+  }
+})
 
 export const Preview = styled('div', {})

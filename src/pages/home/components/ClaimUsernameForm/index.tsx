a0cce3bbc38f3f29e0c5032d927d97452bea4162
root <root@Arthur>
2023-09-12 11:34:45 -0300

    utilizando react hook form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,33 @@
 import { Button, TextInput } from "@ignite-ui/react"
 import { Form } from "./styles"
 import { ArrowRight } from "phosphor-react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const claimUsernameFormSchema = z.object({
+  username: z.string().min(3).max(20),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameFrom() {
-  return <Form>
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  return <Form
+    as="form"
+    onSubmit={handleSubmit(handleClaimUsername)}
+  >
     <TextInput
       size="sm"
       prefix="ignite.com/"
       placeholder="username"
-      crossOrigin={undefined} />
+      crossOrigin={undefined}
+      {...register("username")}
+    />
     <Button
       size="sm"
       type="submit"
